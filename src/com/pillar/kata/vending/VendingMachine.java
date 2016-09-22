105efb8fe09905e921f42dff0646b5d827aff69a
hariharana87 <hariharana87@gmail.com>
2016-09-22 15:00:58 -0500

    First commit. VendingMachine class and test class written.

---
+++ src/com/pillar/kata/vending/VendingMachine.java
@@ -0,0 +1,12 @@
+package com.pillar.kata.vending;
+
+/**
+ * The {@code VendingMachine} class emulates the actions of the vending machine
+ * as per the kata description.
+ * @author Hariharan
+ */
+public class VendingMachine {
+
+    
+    
+}
---
+++ test/com/pillar/kata/vending/tests/VendingMachineTest.java
@@ -0,0 +1,34 @@
+package com.pillar.kata.vending.tests;
+
+import com.pillar.kata.vending.VendingMachine;
+import org.junit.After;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the VendingMachine.
+ * @author Hariharan
+ */
+public class VendingMachineTest {
+    
+    public VendingMachineTest() {
+    }
+    
+    VendingMachine vendingMachine;    
+    
+    @Before
+    public void setUp() {
+        vendingMachine = new VendingMachine();
+    }
+    
+    @After
+    public void tearDown() {
+        vendingMachine = null;// remove the reference for the vending machine for garbage collection.
+        // TODO: add any finalizing code required later
+    }
+
+   
+  
+}
